33150f611229f3dfbb0f499938c03ba36d2e9c5d
user <user@DESKTOP-DHSI56N.lan>
2025-06-26 10:01:45 -0700

    printstatement changes

--- src/test/java/gitpractice/Demo.java
+++ src/test/java/gitpractice/Demo.java
@@ -3,7 +3,7 @@ package gitpractice;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("practice github oush");
+		System.out.println("practice github push");
 
 	}
 
