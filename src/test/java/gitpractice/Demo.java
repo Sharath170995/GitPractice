c9dd8adc424d1a29cc176bc3487a8e00cafe3a53
user <user@DESKTOP-DHSI56N.lan>
2025-06-26 10:12:15 -0700

    comment

--- src/test/java/gitpractice/Demo.java
+++ src/test/java/gitpractice/Demo.java
@@ -4,6 +4,9 @@ public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("practice github push");
+		System.out.println("Helloe");
+		int aa = 10;
+		//cnt+1 =quick fix ;
 
 	}
 
